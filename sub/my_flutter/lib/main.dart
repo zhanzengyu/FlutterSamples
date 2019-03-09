f44ac344ca6de4a59f76752eca2dd2d42df15a2a
nesger.zhan <nesger.zhan@dji.com>
2019-03-09 15:47:02 +0800

    implementation two

--- sub/my_flutter/lib/main.dart
+++ sub/my_flutter/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xFFF0F1F0),
         body: Center(
           child: _buildWidget(),
         ),
@@ -16,7 +17,39 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _buildWidget() {
-    //TODO
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Image.asset('assets/images/refresh.png', width: 49, height: 44,),
+          SizedBox(
+            height: 42.0,
+          ),
+          FlatButton(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0),
+              //注意这里 alpha 最大值是 255， sketch 上面最大值是 100
+              color: Color.fromARGB(255, 13, 46, 172),
+              //这里 onPressed 不能为 null，如果写 null 会怎样，大家可以试下~
+              onPressed: (){},
+              child: Text(
+                '刷新',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600
+                ),
+              )
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
   }
 
 }
