d76ba87560536c3d7ac8f4cfdd7b565db7aab7b5
nesger.zhan <nesger.zhan@dji.com>
2019-03-17 16:46:14 +0800

    add main.dart template

--- sub/my_flutter/lib/main.dart
+++ sub/my_flutter/lib/main.dart
@@ -1,22 +1,40 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(_widgetForRoute(window.defaultRouteName));
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: _buildWidget(),
+Widget _widgetForRoute(String route) {
+  switch (route) {
+    case 'route1':
+      return MaterialApp(
+        home: Scaffold(
+          body: _buildWidget1(),
         ),
-      ),
-    );
+      );
+    case 'route2':
+      return MaterialApp(
+        home: Scaffold(
+          body: _buildWidget2(),
+        ),
+      );
+    default:
+      return MaterialApp(
+        home: Scaffold(
+          body: _buildDefaultWidget(),
+        ),
+      );
   }
+}
 
-  Widget _buildWidget() {
-    //TODO
-  }
+Widget _buildWidget1() {
+  //TODO
+}
 
+Widget _buildWidget2() {
+  //TODO
+}
+
+Widget _buildDefaultWidget() {
+  //TODO
 }
