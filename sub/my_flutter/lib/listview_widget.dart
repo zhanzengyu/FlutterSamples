81a69e84942a222c77484c9218887ebda5d99d3e
zengyu.zhan <zengyu.zhan@dji.com>
2020-03-21 17:31:41 +0800

    add multi layout

--- sub/my_flutter/lib/listview_widget.dart
+++ sub/my_flutter/lib/listview_widget.dart
@@ -24,6 +24,8 @@ class _ListViewWidgetState extends State<ListViewWidget> {
 
   /// 实际场景可能是从网络拉取，这里演示就直接填充数据源了
   void _initData() {
+    itemBeans.add(ItemBean('', ''));
+
     itemBeans.add(ItemBean('第一句', '关注微信公众号「AndroidTraveler」'));
     itemBeans.add(ItemBean('第二句', '星河滚烫，你是人间理想'));
     itemBeans.add(ItemBean('第三句', '我明白你会来，所以我等。'));
@@ -37,7 +39,14 @@ class _ListViewWidgetState extends State<ListViewWidget> {
       child: ListView.builder(
         itemCount: itemBeans.length,
         itemBuilder: (context, index) {
-          return ItemWidget(index, itemBeans[index], widget.listener);
+          if (index == 0) {
+            return Container(
+              color: Colors.blue,
+              height: 66,
+            );
+          } else {
+            return ItemWidget(index, itemBeans[index], widget.listener);
+          }
         },
       ),
     );
